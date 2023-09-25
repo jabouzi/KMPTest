64a52b170f58a780457d8faf1569fd7ecea8209e
Skander Jabouzi <skander.jabouzi@telus.com>
2023-09-24 22:00:53 -0400

    add missing files

--- iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-resources.sh
+++ iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-resources.sh
@@ -97,11 +97,9 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/../../shared/src/commonMain/resources/compose-multiplatform.xml"
   install_resource "${PODS_ROOT}/../../shared/src/commonMain/resources/font"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/../../shared/src/commonMain/resources/compose-multiplatform.xml"
   install_resource "${PODS_ROOT}/../../shared/src/commonMain/resources/font"
 fi
 
